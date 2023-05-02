4836a16e849ea8f8cedbc4a2d1d8dd1421c1d1a4
bri92a <bri92a@gmail.com>
2023-05-02 17:27:25 -0500

    Initial Commit

---
+++ Academy Interactive Map/Academy_Interactive_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Academy_Interactive_MapApp.swift
+//  Academy Interactive Map
+//
+//  Created by Brian Winandy on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Academy_Interactive_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Academy Interactive MapUITests/Academy_Interactive_MapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Academy_Interactive_MapUITestsLaunchTests.swift
+//  Academy Interactive MapUITests
+//
+//  Created by Brian Winandy on 5/2/23.
+//
+
+import XCTest
+
+final class Academy_Interactive_MapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
