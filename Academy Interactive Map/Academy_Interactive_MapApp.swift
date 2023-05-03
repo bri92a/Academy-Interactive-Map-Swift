f561a0ddcdcca471fa72b93f08bf234c4cd06cc2
bri92a <bri92a@gmail.com>
2023-05-03 11:10:39 -0500

    Added Interactive Map Framework Dependency

--- Academy Interactive Map/Academy_Interactive_MapApp.swift
+++ Academy Interactive Map/Academy_Interactive_MapApp.swift
@@ -5,14 +5,3 @@
 //  Created by Brian Winandy on 5/2/23.
 //
 
-import SwiftUI
-
-@main
-struct Academy_Interactive_MapApp: App {
-    var body: some Scene {
-        WindowGroup {
-            
-            ContentView()
-        }
-    }
-}
