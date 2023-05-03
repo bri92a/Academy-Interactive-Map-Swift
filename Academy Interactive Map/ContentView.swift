0ba586502c2918d555f5b3075535dc64b8c88745
bri92a <bri92a@gmail.com>
2023-05-03 12:17:21 -0500

    Added Interactive Map features to ContentView

--- Academy Interactive Map/ContentView.swift
+++ Academy Interactive Map/ContentView.swift
@@ -6,21 +6,37 @@
 //
 
 import SwiftUI
+import InteractiveMap
 
 struct ContentView: View {
+    
+    @State private var clickedPath = PathData()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        InteractiveMap(svgName: "tr") { pathData in // or just use $0
+            InteractiveShape(pathData)
+                .initWithAttributes()
+            VStack {
+                Text(clickedPath.name.isEmpty ? "" : "\(clickedPath.name) is clicked!" )
+                    .font(.largeTitle)
+                    .padding(.bottom, 15)
+                InteractiveMap(svgName: "tr") { pathData in
+                    InteractiveShape(pathData)
+                        .initWithAttributes()
+                        .shadow(color: clickedPath == pathData ? .white : .clear, radius: 6)
+                        .onTapGesture {
+                            clickedPath = pathData
+                        }
+                        .scaleEffect(clickedPath == pathData ? 1.4 : 1)
+                        .animation(.easeInOut(duration: 0.2), value: clickedPath)
+                        .zIndex(clickedPath == pathData ? 2 : 1)
+                }
+            }
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
